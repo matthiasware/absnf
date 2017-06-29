422fb50c4364ae96927b2fc8ccfdfb3cf68f3871
sa26lat <matthias.mitterreiter@uni-jena.de>
2017-06-29 02:57:24 +0200

    presentation

--- code/num_blocksize.cu
+++ code/num_blocksize.cu
@@ -77,6 +77,56 @@ void gridsize()
 	cudaFree(d_A);
 	cudaFree(d_dz);
 }
+void blocksize()
+{
+	cudaDeviceProp prop;
+	int devcount;
+	cudaGetDeviceCount(&devcount);
+	cudaGetDeviceProperties(&prop, 0);
+
+	int s = 16000;
+	
+	// t_def *h_L = (t_def *)malloc(s*s*sizeof(t_def));
+	t_def *h_dz = (t_def *)malloc(s*sizeof(t_def));
+	t_def *h_A = (t_def *)malloc(s*s*sizeof(t_def));
+
+	utils::fillRandVector(h_dz, s,-10,10);
+	utils::fillRandVector(h_A, s*s,-10,10);
+
+	// t_def *d_L; cudaMalloc((void **)&d_L, s*s*sizeof(t_def));
+	t_def *d_dz; cudaMalloc((void **)&d_dz, s*sizeof(t_def));
+	// t_def *d_Tss; cudaMalloc((void **)&d_Tss, s*s*sizeof(t_def));
+	t_def *d_A; cudaMalloc((void **)&d_A, s*s*sizeof(t_def));
+
+	TimeVar t_1;
+	TimeVar t_0;	
+	
+	t_0 = std::chrono::high_resolution_clock::now();
+	cudaMemcpy(d_A, h_A,  s*s*sizeof(t_def), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_dz, h_dz, s*sizeof(t_def), cudaMemcpyHostToDevice);	
+	t_1 = std::chrono::high_resolution_clock::now();
+	auto time_mem = std::chrono::duration_cast<std::chrono::microseconds>( t_1 - t_0 ).count();
+	std::cout << "Memory upload: " << time_mem << std::endl;
+
+	// std::cout << prop.maxThreadsPerBlock << std::endl;
+	int gs;
+	int bs = prop.maxThreadsPerBlock
+	for(int i=1; i<=1000; i+=1)
+	{	
+		gs = i;
+		t_0 = std::chrono::high_resolution_clock::now();
+		absnf::multWithDz<<<gs, bs>>>(d_A, d_dz, s);
+		cudaDeviceSynchronize();
+		t_1 = std::chrono::high_resolution_clock::now();
+		auto time_exec = std::chrono::duration_cast<std::chrono::microseconds>( t_1 - t_0 ).count();
+		std::cout << "[" << gs << "," << time_exec << "]," << std::endl;
+	}
+
+	free(h_A);
+	free(h_dz);
+	cudaFree(d_A);
+	cudaFree(d_dz);
+}
 void block_grid()
 {
 	std::ofstream myfile;
@@ -145,6 +195,6 @@ void block_grid()
 // 0.1 - 1
 int main()
 {
-	gridsize();
+	blocksize();
 	return 0;
 }
