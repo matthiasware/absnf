0282574bb7127c7f0f90a72dd4de23e6570f9c86
Matthias Mitterreiter <matthias@wolowitz.inf-i2.uni-jena.de>
2017-07-04 21:45:58 +0200

    cache misses

--- code/python/test_grid_block_algorithms.py
+++ code/python/test_grid_block_algorithms.py
@@ -5,165 +5,6 @@ from matplotlib.colors import LinearSegmentedColormap
 import random
 from matplotlib.offsetbox import AnchoredText
 
-# class cuda_thread():
-#     def __init__(self, thread_id, block_id, matrix, 
-#                  s, block_dim, grid_dim):
-#         self.thread_id = thread_id
-#         self.block_id = block_id
-#         self.matrix = matrix
-#         self.s = s
-#         self.i = thread_id
-#         self.j = block_id
-#         self.block_dim = block_dim
-#         self.grid_dim = grid_dim
-#         self.id = self.i * self.s + self.j;
-#         self.done = False
-#         self.started = False
-#         self.global_id = thread_id + block_id * block_dim
-
-#     def update(self):
-#         if self.id < self.s*self.s and self.j<self.s:
-#             self.matrix[self.id] = self.global_id
-#             self.i += self.block_dim
-#             if self.i>=self.s:
-#                 self.i = self.i%self.s
-#                 self.j = self.j + self.grid_dim
-#             self.id = self.i*self.s + self.j
-#         else:
-#             self.done = True
-
-# class cuda_thread2():
-#     def __init__(self, thread_id, block_id, matrix, 
-#                  s, block_dim, grid_dim):
-#         self.thread_id = thread_id
-#         self.block_id = block_id
-#         self.matrix = matrix
-#         self.s = s
-#         self.i = thread_id
-#         self.j = block_id
-#         self.block_dim = block_dim
-#         self.grid_dim = grid_dim
-#         self.id = self.i * self.s + self.j;
-#         self.done = False
-#         self.started = False
-#         self.global_id = thread_id + block_id * block_dim
-
-#     def update(self):
-#         if self.id < self.s*self.s and self.j<self.s:
-#             if self.i < self.s:
-#                 self.matrix[self.id] = self.global_id
-#                 self.i += self.block_dim
-#             else:
-#                 self.i = self.i%self.s
-#                 self.j = self.j + self.grid_dim
-#             self.id = self.i*self.s + self.j
-#         else:
-#             self.done = True
-
-
-class image_generator():
-    def __init__(self,block_dim, grid_dim, matrix,s, inplace=True, multicores=None):
-        self.threads = []
-        self.current_thread = 0
-        self.inplace = inplace
-        self.matrix = matrix
-        self.i = 0
-        if multicores:
-            self.multicores = multicores
-        else:
-            self.multicores = block_dim*grid_dim
-        for block_id in range(grid_dim):
-            for thread_id in range(block_dim):
-                thread = cuda_thread(thread_id, block_id, matrix,s, block_dim, grid_dim)
-                self.threads.append(thread)
-            self.all_done = np.zeros(len(self.threads), dtype=bool)
-            self.next = True
-        self.matrix_time = []
-
-    # def calculateNextThread(self,current):
-    #     thread = self.theads[]
-
-    # def generate(self):
-    #     print(self.i)
-    #     self.i += 1
-    #     if not self.next:
-    #         return
-    #     if not self.inplace:
-    #         self.matrix_time.append(np.copy(self.matrix))
-    #     for i in range(self.multicores):
-    #         thread = self.threads[self.current_thread]
-    #         thread.update()
-    #         self.current_thread = 
-    #         if thread.done:
-    #             self.all_done[self.current_thread] = thread.done
-    #             self.current_thread = (self.current_thread + 1) % len(self.threads)
-    #     if np.all(self.all_done):
-    #         self.next = False
-
-    def generate(self):
-        print(self.i)
-        self.i += 1
-        if not self.next:
-            return
-        if not self.inplace:
-            self.matrix_time.append(np.copy(self.matrix))
-        for i in range(self.multicores):
-            thread = self.threads[self.current_thread]
-            thread.update()
-            if thread.done:
-                self.all_done[self.current_thread] = thread.done
-                self.current_thread = (self.current_thread + 1) % len(self.threads)
-        if np.all(self.all_done):
-            self.next = False
-
-class image_generator2():
-    def __init__(self,block_dim, grid_dim, matrix,s, warpsize):
-        self.threads = []
-        self.block_dim = block_dim
-        self.grid_dim = grid_dim
-        self.current_thread = 0
-        self.matrix = matrix
-        self.warpsize = warpsize
-        self.i = 0
-        for block_id in range(grid_dim):
-            for thread_id in range(block_dim):
-                thread = cuda_thread(thread_id, block_id, matrix,s, block_dim, grid_dim)
-                self.threads.append(thread)
-            self.all_done = np.zeros(len(self.threads), dtype=bool)
-            self.next = True
-        self.matrix_time = []
-    
-    def getThread(self,threadlist):
-        if len(threadlist) > 0:
-            t = threadlist.pop()
-            if not t.done:
-                return t
-            else:
-                return None
-        else:
-            return None
-
-    def generate(self):
-        print("RUN:",self.i)
-        self.i += 1
-        if not self.next:
-            return
-        # iteratre over blocks
-        for i in range(self.grid_dim):
-            block_threads = [thread for thread in self.threads if thread.block_id == i]
-            block_threads = [thread for thread in block_threads if not thread.done]
-            for w in range(self.warpsize):
-                # chose thread to execute
-                thread = self.getThread(block_threads)
-                if thread:
-                    thread.update()
-                    print(thread.block_id, thread.thread_id, thread.global_id)
-                else:
-                    break
-        #
-        self.all_done = np.array([thread.done for thread in self.threads], dtype=bool)
-        if np.all(self.all_done):
-            self.next = False
 
 class CudaThread():
     def __init__(self, thread_id, block_id, matrix, 
@@ -249,6 +90,7 @@ class MPU():
         for thread_id in range(block_dim):
             thread = self.thread_class(thread_id, block_id, matrix, s, block_dim, grid_dim, self.verbose)
             self.threads.append(thread)
+        random.shuffle(self.threads)
         if self.verbose:
             print("MPU: ", self.id, "creating threads for block: ", block_id, " : ", len(self.threads))
         self.checkWork()
@@ -263,6 +105,8 @@ class MPU():
                         if threads:
                             thread = threads.pop()
                             warp_threads.append(thread)
+                    if self.verbose:
+                        print("MPU ", self.id, "Starting new warp!")
                     warp.setThreads(warp_threads)
                 warp.execute()
             self.checkWork()
@@ -285,6 +129,7 @@ class GPUDevice():
         self.done = True
         self.verbose = verbose
         self.name = name
+        self.iterations = 0
 
     def setTask(self, thread_class, grid_dim, block_dim, matrix, s):
         self.matrix = matrix
@@ -294,11 +139,13 @@ class GPUDevice():
         self.blocks = [block_id for block_id in range(grid_dim)]
         self.thread_class = thread_class
         self.done = False
+        self.iterations = 0
 
     def execute(self):
         if not self.done:
+            self.iterations += 1
             if self.verbose:
-                print("GPU: Next Iteration!")
+                print("-"*20, "GPU ", self.name, " Iteration: ", self.iterations, "-"*20)
             for mpu in self.mpus:
                 if mpu.done:
                     if self.blocks:
@@ -329,14 +176,16 @@ def animate(update_creator, frames, data):
 
 # DEVICE
 NUM_MPU = 4
-NUM_WARP_PER_MPU = 8
+NUM_WARP_PER_MPU = 16
 NUM_THREADS_PER_WARP= 8
 NUM_CONCURRENT_THREADS = NUM_MPU * NUM_WARP_PER_MPU * NUM_THREADS_PER_WARP
 
 gpu = GPUDevice(NUM_MPU, NUM_WARP_PER_MPU, NUM_THREADS_PER_WARP, "GTX", True)
 
 # Chose dimensions
-BLOCK_DIM = NUM_WARP_PER_MPU * NUM_THREADS_PER_WARP
+# BLOCK_DIM = NUM_WARP_PER_MPU * NUM_THREADS_PER_WARP
+# IF BLOCKDIM IS NOT EQUALS TO WARPS * THEADS -> INFINATELY MANY CACHE MISSES!!!
+BLOCK_DIM = 256
 GRID_DIM = NUM_MPU
 
 # Prepare Data
